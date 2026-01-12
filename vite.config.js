bce94c8368a6fad343e31554a9fa60f356868822
Hamzaelhamdani <62265033+Hamzaelhamdani@users.noreply.github.com>
2026-01-12 18:19:57 +0100

    Fix Netlify build - update sourcemap config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    sourcemap: 'inline-cheap-module-source-map'
+    sourcemap: false
   },
   server: {
     hmr: {
